1e33e4418cc63d55a8e0a09869ca1af04385c014
lukashanes-cyper <lukas.hanes@gmail.com>
2026-05-21 14:17:40 +0200

    aa
    aa

---
+++ Player.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Player {
+
+
+
+        private char marker;
+
+        public Player(char marker) {
+            this.marker = marker;
+        }
+
+        public char getMarker() {
+            return marker;
+        }
+    }
+
