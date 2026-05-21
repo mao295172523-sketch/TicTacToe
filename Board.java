cf5445abd8459ac5064daf892f1a385d49a825e2
lukashanes-cyper <lukas.hanes@gmail.com>
2026-05-21 14:05:23 +0200

    Ausgabe
    Ausgabe

---
+++ Board.java
@@ -0,0 +1,56 @@
+package org.example;
+
+public class Board {
+
+        cells = new char[3][3];
+        clear();
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        return cells[x][y] == ' ';
+    }
+
+    public void place(int x, int y, char marker) {
+        if (isCellEmpty(x, y)) {
+            cells[x][y] = marker;
+        }
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (cells[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void clear() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print() {
+        System.out.println("Current Board:");
+        System.out.println("-----------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" " + cells[i][j]);
+                if (j < 2) System.out.print("|");
+            }
+            System.out.print("|");
+            System.out.println();
+            //if (i < 2) System.out.println("   |   |   ");
+        }
+        System.out.println("-----------");
+    }
+}
+
+
+
